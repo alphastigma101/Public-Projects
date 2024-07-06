33b9182c13bb707a9b496d899579b4702b300d6d
Logan Welty <12logan.welty@gmail.com>
2024-07-05 21:08:30 -0700

    I implemented the scanner and now I need to clean up the code and remove the undeeded files

--- scanner.cc
+++ scanner.cc
@@ -1,12 +1,11 @@
 #include "scanner.h"
-#include "languages.h"
-
 
 Scanner::Scanner(std::string Source): Source(this->Source) {
     if (tokens == (void*)0) { tokens = new std::vector<>(); }
 }
 
 const std::map<std::string, TokenType> Scanner::keywords = {
+    // Add all the Keywrods here
     {"and",    TokenType::AND},
     {"class",  TokenType::CLASS},
     {"else",   TokenType::ELSE},
@@ -50,89 +49,89 @@ void Scanner::scanToken() {
         // Going to add some threading as well 
 
         // C/C++
-        case 'i': if (match('n') && match('t')) addToken(INT); break;
-        case 'f': if (match('l') && match('o') && match('a') && match('t')) addToken(C_FLOAT); break;
+        case 'i': if (match('n') && match('t')) addToken(TokenType::INT); break;
+        case 'f': if (match('l') && match('o') && match('a') && match('t')) addToken(TokenType::FLOAT); break;
         // Add cases for other C/C++ tokens
         // Java
-        case 'p': if (match('u') && match('b') && match('l') && match('i') && match('c')) addToken(PUBLIC); break;
+        case 'p': if (match('u') && match('b') && match('l') && match('i') && match('c')) addToken(TokenType::PUBLIC); break;
         // Add cases for other Java tokens
         // Python
-        case 'd': if (match('e') && match('f')) addToken(DEF); break;
+        case 'd': if (match('e') && match('f')) addToken(TokenType::DEF); break;
         // Add cases for other Python tokens
         // JavaScript
-        case 'v': if (match('a') && match('r')) addToken(VAR); break;
+        case 'v': if (match('a') && match('r')) addToken(TokenType::VAR); break;
         // Add cases for other JavaScript tokens
         // Ruby
         // Add cases for other Ruby tokens
         // Swift
-        case 'f': if (match('u') && match('n') && match('c')) addToken(FUNC); break;
+        case 'f': if (match('u') && match('n') && match('c')) addToken(TokenType::FUNC); break;
         // Add cases for other Swift tokens
         // Go
         // Add cases for other Go tokens
         // Kotlin
-        case 'f': if (match('u') && match('n')) addToken(Token::FUN); break;
+        case 'f': if (match('u') && match('n')) addToken(TokenType::FUN); break;
         // Add cases for other Kotlin tokens
         // Scala
         // Add cases for other Scala tokens
         // TypeScript
         // Add cases for other TypeScript tokens
         // PHP
-        case 'f': if (match('u') && match('n') && match('c') && match('t') && match('i') && match('o') && match('n')) addToken(FUNCTION); break;
+        case 'f': if (match('u') && match('n') && match('c') && match('t') && match('i') && match('o') && match('n')) addToken(TokenType::FUNCTION); break;
         // Add cases for other PHP tokens
         // R
         // Add cases for other R tokens
         // Objective-C
-        case '@': if (match('i') && match('n') && match('t')) addToken(OBJC_INT); break;
+        case '@': if (match('i') && match('n') && match('t')) addToken(TokenType::INT); break;
         // Add cases for other Objective-C tokens
         // Rust
-        case 'f': if (match('n')) addToken(FN); break;
+        case 'f': if (match('n')) addToken(TokenType::FN); break;
         // Add cases for other Rust tokens
         // Dart
         // Add cases for other Dart tokens
         // Lua
         // Add cases for other Lua tokens
         // Shell
-        case 'i': if (match('f')) addToken(IF); break;
+        case 'i': if (match('f')) addToken(TokenType::IF); break;
         // Add cases for other Shell tokens
         // HTML
-        case '<': addToken(HTML_TAG_OPEN); break;
+        case '<': addToken(TokenType::TAG_OPEN); break;
         // Add cases for other HTML tokens
         // CSS
-        case '.': addToken(CSS_SELECTOR); break;
+        case '.': addToken(TokenType::SELECTOR); break;
         // Add cases for other CSS tokens
         // SQL
-        case 'S': if (match('E') && match('L') && match('E') && match('C') && match('T')) addToken(SQL_SELECT); break;
+        case 'S': if (match('E') && match('L') && match('E') && match('C') && match('T')) addToken(TokenType::SELECT); break;
         // Add cases for other SQL tokens
         // MATLAB
         // Add cases for other MATLAB tokens
         // Verilog
-        case 'm': if (match('o') && match('d') && match('u') && match('l') && match('e')) addToken(VERILOG_MODULE); break;
+        case 'm': if (match('o') && match('d') && match('u') && match('l') && match('e')) addToken(TokenType::MODULE); break;
         // Add cases for other Verilog tokens
         // VHDL
-        case 'e': if (match('n') && match('t') && match('i') && match('t') && match('y')) addToken(VHDL_ENTITY); break;
+        case 'e': if (match('n') && match('t') && match('i') && match('t') && match('y')) addToken(TokenType::ENTITY); break;
         // Add cases for other VHDL tokens
         // F#
-        case 'l': if (match('e') && match('t')) addToken(LET); break;
+        case 'l': if (match('e') && match('t')) addToken(TokenType::LET); break;
         // Add cases for other F# tokens
         // C#
         // Add cases for other C# tokens
         // VBA
-        case 'S': if (match('u') && match('b')) addToken(VBA_SUB); break;
+        case 'S': if (match('u') && match('b')) addToken(TokenType::SUB); break;
         // Add cases for other VBA tokens
         // Fortran
-        case 'i': if (match('n') && match('t') && match('e') && match('g') && match('e') && match('r')) addToken(INTEGER); break;
+        case 'i': if (match('n') && match('t') && match('e') && match('g') && match('e') && match('r')) addToken(TokenType::INTEGER); break;
         // Add cases for other Fortran tokens
         // COBOL
-        case 'I': if (match('D') && match('E') && match('N') && match('T') && match('I') && match('F') && match('I') && match('C') && match('A') && match('T') && match('I') && match('O') && match('N')) addToken(IDENTIFICATION); break;
+        case 'I': if (match('D') && match('E') && match('N') && match('T') && match('I') && match('F') && match('I') && match('C') && match('A') && match('T') && match('I') && match('O') && match('N')) addToken(TokenType::IDENTIFICATION); break;
         // Add cases for other COBOL tokens
         // Pascal
-        case 'p': if (match('r') && match('o') && match('g') && match('r') && match('a') && match('m')) addToken(PROGRAM); break;
+        case 'p': if (match('r') && match('o') && match('g') && match('r') && match('a') && match('m')) addToken(TokenType::PROGRAM); break;
         // Add cases for other Pascal tokens
         // LISP
-        case 'd': if (match('e') && match('f') && match('u') && match('n')) addToken(DEFUN); break;
+        case 'd': if (match('e') && match('f') && match('u') && match('n')) addToken(TokenType::DEFUN); break;
         // Add cases for other LISP tokens
         // Scheme
-        case 'd': if (match('e') && match('f') && match('i') && match('n') && match('e')) addToken(DEFINE); break;
+        case 'd': if (match('e') && match('f') && match('i') && match('n') && match('e')) addToken(TokenType::DEFINE); break;
         // Add cases for other Scheme tokens
         // Groovy
         // Add cases for other Groovy tokens
@@ -151,31 +150,31 @@ void Scanner::scanToken() {
         case 'b': if (match('e') && match('g') && match('i') && match('n')) addToken(BEGIN); break;
 
         // Single-character tokens
-        case '(': addToken(Token::LEFT_PAREN); break;
-        case ')': addToken(Token::RIGHT_PAREN); break;
-        case '{': addToken(Token::LEFT_BRACE); break;
+        case '(': addToken(TokenType::LEFT_PAREN); break;
+        case ')': addToken(TokenType::RIGHT_PAREN); break;
+        case '{': addToken(TokenType::LEFT_BRACE); break;
         case '}': addToken(Token::RIGHT_BRACE); break;
-        case ',': addToken(Token::COMMA); break;
-        case '.': addToken(Token::DOT); break;
-        case '-': addToken(Token::MINUS); break;
-        case '+': addToken(Token::PLUS); break;
-        case ';': addToken(Token::SEMICOLON); break;
-        case '*': addToken(Token::STAR); break;
-        case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
-        case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
-        case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
-        case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+        case ',': addToken(TokenType::COMMA); break;
+        case '.': addToken(TokenType::DOT); break;
+        case '-': addToken(TokenType::MINUS); break;
+        case '+': addToken(TokenType::PLUS); break;
+        case ';': addToken(TokenType::SEMICOLON); break;
+        case '*': addToken(TokenType::STAR); break;
+        case '!': addToken(match('=') ? TokenType::BANG_EQUAL : TokenType::BANG); break;
+        case '=': addToken(match('=') ? TokenType::EQUAL_EQUAL : TokenType::EQUAL); break;
+        case '<': addToken(match('=') ? TokenType::LESS_EQUAL : TokenType::LESS); break;
+        case '>': addToken(match('=') ? TokenType::GREATER_EQUAL : TokenType::GREATER); break;
         case '/':
                if (match('/')) {
                    // A comment goes until the end of the line.
                    while (peek() != '\n' && !isAtEnd()) advance();
                } else {
-                   addToken(Token::SLASH);
+                   addToken(TokenType::SLASH);
                }
                break;
         case 'o':
                // Need to also add support for | and || 
-               if (match('r')) { addToken(OR); }
+               if (match('r')) { addToken(TokenType::OR); }
                break;
         case ' ':
         case '\r':
@@ -241,13 +240,16 @@ void Scanner::number() {
 }
 
 char Scanner::peekNext() {
-    if (current + 1 >= source.length()) return '\0';
-    return source.charAt(current + 1);
+    if (current + 1 >= Source.length()) return '\0';
+    return Source.at(current + 1);
 }
 
 void Scanner::identifier() {
     while (isAlphaNumeric(peek())) advance();
-    addToken(IDENTIFIER);
+    std::string text = Source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == (void*)0) type = Token::IDENTIFIER;
+    addToken(type);
 }
 
 bool Scanner::isAlpha(char c) {
